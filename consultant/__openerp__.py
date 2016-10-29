5b7cf50ecaf95f0f934c685dca0c03f039f75369
Riyaj Pathan <rjpathan19@gmail.com>
2016-10-29 19:48:50 +0530

    add module version & correct module dependency

--- consultant/__openerp__.py
+++ consultant/__openerp__.py
@@ -1,8 +1,9 @@
 {
   'name': 'Consultant Application',
+  'version': '9.0.1',
   'description': 'Manage consultants and search for knowledge',
   'author': 'Martin WIlderoth',
-  'depends': ['hr'],
+  'depends': ['hr', 'crm'],
   'application': True,
   'data': [ 'views/consultant_consult.xml' ],
 }
