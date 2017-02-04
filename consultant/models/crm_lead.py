13b4e1ade5e63675d12c3c4719a53bd16b0b9ed8
Riyaj Pathan <rjpathan19@gmail.com>
2017-02-04 09:35:38 +0530

    add Consultants Profile search fields in Opportunity & Search functionality

--- consultant/models/crm_lead.py
+++ consultant/models/crm_lead.py
@@ -22,6 +22,12 @@ class crm_lead(models.Model):
     consultant_ids = fields.Many2many('consultant.consult', 'consultant_consult_opportunity_rel', 'opportunity_id', 'consultant_id', 'Consultants')
     consultant_count = fields.Integer(compute='_consultant_count', string='# Consultants', copy=False, default=0)
 
+    #consultant profile search fields:
+    industry_ids = fields.Many2many('consultant.industry', string='Industry')
+    role_ids = fields.Many2many('consultant.role', string='Role')
+    certificate_ids = fields.Many2many('consultant.certificate', string='Certifications')
+    competence_ids = fields.Many2many('consultant.competence', string='IT Competence')
+
     @api.multi
     def action_open_consultants(self):
         """
@@ -37,5 +43,62 @@ class crm_lead(models.Model):
         for r in result:
             r = r[0]
             consultants.append(r)
+        res['domain'] = [['id', 'in', consultants]]
+        return res
+
+    @api.multi
+    def action_search_consultants(self):
+        """Search Consultants by Industry | Role | Competence | Certificate & show in list view
+        """
+        res = self.env['ir.actions.act_window'].for_xml_id('consultant', 'action_consultant_consult')
+        consultants = []
+        industries, roles, competences, certificates = [], [], [], []
+        for oppr in self:
+            for industry in oppr.industry_ids:
+                industries.append(industry.id)
+            if industries:
+                industries = ', '.join(str(ind) for ind in industries)
+                oppr._cr.execute(""" select consultant_consult_id from consultant_consult_consultant_industry_rel \
+                                        where consultant_industry_id in (%s)"""%(industries))
+                result = self._cr.fetchall()
+                for r in result:
+                    r = r[0]
+                    consultants.append(r)
+
+            for role in oppr.role_ids:
+                roles.append(role.id)
+            if roles:
+                roles = ', '.join(str(rl) for rl in roles)
+                oppr._cr.execute("""select consultant_consult_id from consultant_consult_consultant_role_rel \
+                                        where consultant_role_id in (%s)"""%(roles))
+                result = self._cr.fetchall()
+                for r in result:
+                    r = r[0]
+                    consultants.append(r)
+
+            for competence in oppr.competence_ids:
+                competences.append(competence.id)
+            if competences:
+                competences = ', '.join(str(cmpt) for cmpt in competences)
+                oppr._cr.execute(""" select consultant_consult_id from consultant_competence_consultant_consult_rel \
+                                         where consultant_competence_id in (%s)"""%(competences))
+                result = self._cr.fetchall()
+                for r in result:
+                    r = r[0]
+                    consultants.append(r)
+
+            for certificate in oppr.certificate_ids:
+                certificates.append(certificate.id)
+            if certificates:
+                certificates = ', '.join(str(cert) for cert in certificates)
+                oppr._cr.execute(""" select consultant_consult_id from consultant_certificate_consultant_consult_rel \
+                                         where consultant_certificate_id in (%s)"""%(certificates))
+                result = self._cr.fetchall()
+                for r in result:
+                    r = r[0]
+                    consultants.append(r)            
+
+            consultants = list(set(consultants))
+
         res['domain'] = [['id', 'in', consultants]]
         return res
