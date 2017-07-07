18e9d76be42c72892e351b25eb2ce3a3eaa110e2
Riyaj Pathan <rjpathan19@gmail.com>
2017-07-08 00:50:59 +0530

    Set Nox Terms Update field to False whenever Terms are updated/changed

--- consultant/models/nox_terms.py
+++ consultant/models/nox_terms.py
@@ -29,6 +29,15 @@ class NoxTerms(models.Model):
             _logger.error("Rule to prevent deletion of record(s) from nox_terms already exists.")
             pass
 
+    @api.multi
+    def write(self, vals):
+        """Set Nox Terms Update field to False whenever Terms are updated/changed.
+        """
+        if 'description' in vals:
+            users = self.env['res.users'].search([('id','>',0)])
+            users.write({'nox_terms_read': False})
+        return super(NoxTerms, self).write(vals)
+
 class ResUsers(models.Model):
     _inherit = "res.users"
 
