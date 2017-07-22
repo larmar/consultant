29dccf0256372912fade100afbc12c3d7b7e5c47
Riyaj Pathan <rjpathan19@gmail.com>
2017-07-22 12:37:33 +0530

    change nox_sum_hours to be computed field by formula ( [all weekdays excluding saturday and sunday in the period from Start Date] X 8 X "Avg FTE %" )

--- nox_opportunity_order/models/crm_lead.py
+++ nox_opportunity_order/models/crm_lead.py
@@ -8,13 +8,36 @@
 
 from odoo import models, fields, api
 
+from datetime import datetime
+from dateutil.rrule import DAILY, rrule, MO, TU, WE, TH, FR
+
+def get_weekdaysrange(start_date, end_date):
+	return list(rrule(DAILY, dtstart=start_date, until=end_date, byweekday=(MO,TU,WE,TH,FR)))
+
+def get_value_percent(value, percent):
+	return value * (percent / 100)
+
 class CrmLead(models.Model):
     _inherit = "crm.lead"
+
+    @api.multi
+    @api.depends('nox_is_startdate', 'nox_is_enddate', 'nox_ftepercent_temp')
+    def _compute_nox_sum_hours(self):
+    	for oppr in self:
+    		oppr.nox_sum_hours = 0.0
+    		if oppr.nox_is_startdate and oppr.nox_is_enddate and oppr.nox_ftepercent_temp and oppr.nox_is_enddate >= oppr.nox_is_startdate:
+    			start_date = datetime.strptime(oppr.nox_is_startdate, "%Y-%m-%d").date()
+    			end_date = datetime.strptime(oppr.nox_is_enddate, "%Y-%m-%d").date()
+    			diffDays = get_weekdaysrange(start_date, end_date)
+    			if diffDays:
+    				nox_sum_hours = get_value_percent((len(diffDays) * 8), oppr.nox_ftepercent_temp)
+    				oppr.nox_sum_hours = nox_sum_hours
+
 
     nox_cost_hourly_rate = fields.Float('Cost hourly rate')
     nox_ftepercent = fields.Float('Avg FTE (%)')
     nox_ftepercent_temp = fields.Float('Avg FTE (%)')
-    nox_sum_hours = fields.Float('Total Hours')
+    nox_sum_hours = fields.Float(string='Total Hours', compute='_compute_nox_sum_hours', store=True)
     nox_sales_hourly_rate = fields.Float('Sales hourly rate')
 
     @api.multi
