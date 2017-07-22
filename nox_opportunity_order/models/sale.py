181fe3af1032eaa29ebf773f00df7611fa1e0f3c
Riyaj Pathan <rjpathan19@gmail.com>
2017-07-22 11:29:19 +0530

    add new fields in Quotation / Sales Order form

--- nox_opportunity_order/models/sale.py
+++ nox_opportunity_order/models/sale.py
@@ -6,3 +6,27 @@
 #
 ##############################################################################
 
+from odoo import models, fields, api
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    nox_is_startdate = fields.Date("Start Date")
+    nox_is_enddate = fields.Date("End Date")
+    nox_cost_hourly_rate = fields.Float('Cost hourly rate')
+    nox_ftepercent = fields.Float('Avg FTE (%)')
+    nox_ftepercent_temp = fields.Float('Avg FTE (%)')
+    nox_sum_hours = fields.Float('Total Hours')
+    nox_sales_hourly_rate = fields.Float('Sales hourly rate')
+
+    nox_contract_signed = fields.Boolean('Contract Signed')
+
+    @api.multi
+    def write(self, vals):
+        if 'nox_ftepercent_temp' in vals:
+            vals['nox_ftepercent'] = vals['nox_ftepercent_temp']
+        return super(SaleOrder, self).write(vals)
+
+    @api.onchange('nox_ftepercent_temp')
+    def onchange_nox_ftepercent(self):
+        self.nox_ftepercent = self.nox_ftepercent_temp
