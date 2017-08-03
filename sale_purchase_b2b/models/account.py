2e738cb774922dc11f057406438159d8f204dd24
Riyaj Pathan <rjpathan19@gmail.com>
2017-08-04 02:01:58 +0530

    set Vendor Bill Date to last day of month assuming month period from 11th day TO 10th day of next month

--- sale_purchase_b2b/models/account.py
+++ sale_purchase_b2b/models/account.py
@@ -7,6 +7,36 @@
 ##############################################################################
 
 from odoo import models, fields, api
+from datetime import datetime
+import calendar
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.model
+    def default_get(self, default_fields):
+    	"""Set Vendor Bill Invoice Date as the last day of the month;
+    	- Month Period = 11th day of month TO 10th day of following month.
+    	"""
+    	res = super(AccountInvoice, self).default_get(default_fields)
+    	if 'type' in res and res['type'] == 'in_invoice':
+    		current_date = datetime.now().date()
+    		year, month = current_date.year, current_date.month
+
+    		if current_date.day <= 10:
+    			month = month - 1
+    		if month == 0:
+    			month = 12
+    			year = year - 1
+
+    		lastDayofMonth = calendar.monthrange(year, month)[1]
+    		
+    		invoice_date = str(year) + '-' + str(month) + '-' + str(lastDayofMonth)
+    		invoice_date = datetime.strptime(invoice_date, '%Y-%m-%d').date()
+    		invoice_date = invoice_date.strftime('%Y-%m-%d')
+
+    		res.update({'date': invoice_date, 'date_invoice': invoice_date})
+    	return res
 
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
