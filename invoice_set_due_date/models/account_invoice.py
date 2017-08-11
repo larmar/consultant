220143b05ddfe527be4e35dcd8103f8c00318462
Riyaj Pathan <rjpathan19@gmail.com>
2017-08-11 11:54:59 +0530

    forbid changing Due Date on Invoice Validate

--- invoice_set_due_date/models/account_invoice.py
+++ invoice_set_due_date/models/account_invoice.py
@@ -11,11 +11,22 @@ from odoo import models, api, fields
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
+    #due_date_lock = fields.Boolean()
+
     @api.onchange('payment_term_id', 'date_invoice')
     def _onchange_payment_term_date_invoice(self):
+        if not self.date_due:
+            return super(AccountInvoice, self)._onchange_payment_term_date_invoice()
+
         date_invoice = self.date_invoice
         if not date_invoice:
             date_invoice = fields.Date.context_today(self)
         if not self.payment_term_id:
             # When no payment term defined
             self.date_due = self.date_due or self.date_invoice
+
+    @api.multi
+    def action_date_assign(self):
+        """Do Nothing: Avoid overwriting Due Date based on Payment Term
+        """
+        return True
