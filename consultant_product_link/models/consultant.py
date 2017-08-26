b6d1a1611c2331841b4fbb2d2923c893aefe3934
Riyaj Pathan <rjpathan19@gmail.com>
2017-08-26 12:58:57 +0530

    auto set consultant stage based on related sales orders expiry date

--- consultant_product_link/models/consultant.py
+++ consultant_product_link/models/consultant.py
@@ -7,6 +7,7 @@
 ##############################################################################
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from datetime import datetime
 
 from odoo.tools.translate import _
 
@@ -36,8 +37,37 @@ class ConsultantConsult(models.Model):
                 sale_order_ids = []
                 temp = [sale_order_ids.append(line.order_id.id) for line in sale_line_ids]
                 consultant.sale_order_ids = sale_order_ids
+                #auto set consultant stage based on related Sales Order's expiration date
+                if sale_order_ids:
+                    consultant.auto_set_consultant_stage()
 
                 purchase_line_ids = self.env['purchase.order.line'].search([('product_id', '=', consultant.product_id.id)])
                 purchase_order_ids = []
                 temp = [purchase_order_ids.append(line.order_id.id) for line in purchase_line_ids]
                 consultant.purchase_order_ids = purchase_order_ids
+
+    @api.multi
+    def auto_set_consultant_stage(self):
+        valid = False
+        today = datetime.now().date()
+        for consultant in self:
+            for order in consultant.sale_order_ids:
+                if order.validity_date:
+                    expiration_date = datetime.strptime(order.validity_date, '%Y-%m-%d').date()
+                    if expiration_date >= today:
+                        valid = True
+                        break
+
+            stage = self.get_consultant_valid_stages(valid)
+            if stage:
+                consultant.write({'stage_id': stage})
+
+    @api.multi
+    def get_consultant_valid_stages(self, valid=False):
+        stage_id = False
+        if valid:
+            #stage_id = self._cr.execute("select id from consultant_stage where name ilike '%%on nox contract%%'")
+            stage_id = self.env['consultant.stage'].search([('name', 'ilike', 'on nox contract')], limit=1)
+        else:
+            stage_id = self.env['consultant.stage'].search([('name', 'ilike', 'sale ready')], limit=1)
+        return stage_id and stage_id.id or False
