d6c5e2e310bc951dda553f54f4c722b52c42ac91
Riyaj Pathan <rjpathan19@gmail.com>
2017-09-18 01:19:02 +0530

    module: add fields Contract Signed, Start Date, End Date in Purchase Order; Set Start Date & End Date from Sale Order if PO is created from SO

---
+++ nox_opportunity_order_purchase/models/purchase.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2016-TODAY Linserv Aktiebolag, Sweden (<http://www.linserv.se>).
+#
+##############################################################################
+
+from odoo import models, fields, api
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    nox_contract_signed = fields.Boolean('Contract Signed', copy=False)
+    nox_is_startdate = fields.Date("Start Date", copy=False)
+    nox_is_enddate = fields.Date("End Date", copy=False)
+
+
+    @api.model
+    def default_get(self, fields):
+        """Set Start Date & End Date from Sales Order when Purchase Order is created from SO
+        """
+        res = super(PurchaseOrder, self).default_get(fields)
+
+        context = self._context
+        if isinstance(context, dict) and context.get('active_model', False) == 'sale.order':
+            Sale = self.env['sale.order'].browse(context['active_ids'])
+            if Sale:
+                res.update({
+                    'nox_is_startdate': Sale.nox_is_startdate,
+                    'nox_is_enddate': Sale.nox_is_enddate,
+                })
+        return res
