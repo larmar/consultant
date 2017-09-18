f283029bd28e72e54c4ac8b902888060c795861e
Riyaj Pathan <rjpathan19@gmail.com>
2017-09-19 01:13:46 +0530

    remove related product 1 & 2 and related fields from Opportunity view

--- nox_opportunity_order/models/crm_lead.py
+++ nox_opportunity_order/models/crm_lead.py
@@ -33,18 +33,6 @@ class CrmLead(models.Model):
     				nox_sum_hours = get_value_percent((len(diffDays) * 8), oppr.nox_ftepercent_temp)
     				oppr.nox_sum_hours = nox_sum_hours
 
-    @api.multi
-    @api.depends('nox_is_startdate', 'nox_is_enddate', 'nox_ftepercent_temp2')
-    def _compute_nox_sum_hours2(self):
-        for oppr in self:
-            oppr.nox_sum_hours2 = 0.0
-            if oppr.nox_is_startdate and oppr.nox_is_enddate and oppr.nox_ftepercent_temp2 and oppr.nox_is_enddate >= oppr.nox_is_startdate:
-                start_date = datetime.strptime(oppr.nox_is_startdate, "%Y-%m-%d").date()
-                end_date = datetime.strptime(oppr.nox_is_enddate, "%Y-%m-%d").date()
-                diffDays = get_weekdaysrange(start_date, end_date)
-                if diffDays:
-                    nox_sum_hours2 = get_value_percent((len(diffDays) * 8), oppr.nox_ftepercent_temp2)
-                    oppr.nox_sum_hours2 = nox_sum_hours2
 
 
     nox_cost_hourly_rate = fields.Float('Cost hourly rate')
@@ -53,33 +41,18 @@ class CrmLead(models.Model):
     nox_sum_hours = fields.Float(string='Total Hours', compute='_compute_nox_sum_hours', store=True)
     nox_sales_hourly_rate = fields.Float('Sales hourly rate')
 
-    nox_cost_hourly_rate2 = fields.Float('Cost hourly rate')
-    nox_ftepercent2 = fields.Float('Avg FTE (%)')
-    nox_ftepercent_temp2 = fields.Float('Avg FTE (%)')
-    nox_sum_hours2 = fields.Float(string='Total Hours', compute='_compute_nox_sum_hours2', store=True)
-    nox_sales_hourly_rate2 = fields.Float('Sales hourly rate')
-
-    nox_product1 = fields.Many2one('product.product', 'Related Product 1')
-    nox_product2 = fields.Many2one('product.product', 'Related Product 2')
-
 
     @api.multi
     def write(self, vals):
         if 'nox_ftepercent_temp' in vals:
             vals['nox_ftepercent'] = vals['nox_ftepercent_temp']
-        if 'nox_ftepercent_temp2' in vals:
-            vals['nox_ftepercent2'] = vals['nox_ftepercent_temp2']
         return super(CrmLead, self).write(vals)
 
     @api.onchange('nox_ftepercent_temp')
     def onchange_nox_ftepercent(self):
         self.nox_ftepercent = self.nox_ftepercent_temp
 
-    @api.onchange('nox_ftepercent_temp2')
-    def onchange_nox_ftepercent2(self):
-        self.nox_ftepercent2 = self.nox_ftepercent_temp2
-
     @api.multi
     def compute_nox_expected_revenue(self):
         for oppr in self:
-            oppr.write({'planned_revenue': (oppr.nox_sales_hourly_rate * oppr.nox_sum_hours) + (oppr.nox_sales_hourly_rate2 * oppr.nox_sum_hours2)})
+            oppr.write({'planned_revenue': (oppr.nox_sales_hourly_rate * oppr.nox_sum_hours)})
