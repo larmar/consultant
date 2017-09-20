2113749c92a5ca02a03847e281519d6eb88dabc2
Riyaj Pathan <rjpathan19@gmail.com>
2017-09-20 20:19:18 +0530

    add button icon link to open Purchase Order

--- sale_purchase_b2b/models/purchase.py
+++ sale_purchase_b2b/models/purchase.py
@@ -26,3 +26,19 @@ class PurchaseOrderLine(models.Model):
         if self.order_id and self.order_id.sale_id:
             self.sale_id = self.order_id.sale_id
             
+    @api.multi
+    def action_open_po(self):
+        for pol in self:
+            name = 'Request for Quotation'
+            if pol.order_id.state not in ('draft', 'sent'):
+                name = 'Purchase Order'
+            
+            return {
+                'name': name,
+                'type': 'ir.actions.act_window',   
+                'res_model': 'purchase.order',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_id': pol.order_id.id,
+                'target': 'current',
+                }
