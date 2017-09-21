0f51845f641f7580c958a6f057402dad5fb7e4a9
Riyaj Pathan <rjpathan19@gmail.com>
2017-09-22 02:11:22 +0530

    redefine function to get sale & purchase orders with Related Product 1, 2 & 3

--- consultant_product_link_extra/models/consultant.py
+++ consultant_product_link_extra/models/consultant.py
@@ -20,6 +20,28 @@ class ConsultantConsult(models.Model):
     product2_id = fields.Many2one('product.product', 'Related Product 2', readonly=True, copy=False)
     product3_id = fields.Many2one('product.product', 'Related Product 3', readonly=True, copy=False)
 
+    @api.multi
+    def _get_orders(self):
+        for consultant in self:
+            if consultant.product_id:
+                sale_line_ids = self.env['sale.order.line'].search(['|','|',
+                                                                ('product_id', '=', consultant.product_id.id), 
+                                                                ('product_id', '=', consultant.product2_id.id), 
+                                                                ('product_id', '=', consultant.product3_id.id)])
+                sale_order_ids = []
+                temp = [sale_order_ids.append(line.order_id.id) for line in sale_line_ids]
+                consultant.sale_order_ids = sale_order_ids
+                #auto set consultant stage based on related Sales Order's expiration date
+                if sale_order_ids:
+                    consultant.auto_set_consultant_stage()
+
+                purchase_line_ids = self.env['purchase.order.line'].search(['|','|',
+                                                                ('product_id', '=', consultant.product_id.id), 
+                                                                ('product_id', '=', consultant.product2_id.id), 
+                                                                ('product_id', '=', consultant.product3_id.id)])
+                purchase_order_ids = []
+                temp = [purchase_order_ids.append(line.order_id.id) for line in purchase_line_ids]
+                consultant.purchase_order_ids = purchase_order_ids
 
     @api.multi
     def action_link_product2(self):
