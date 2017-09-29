d974753453ca74eba036583aa7affdef2c861d4b
Riyaj Pathan <rjpathan19@gmail.com>
2017-09-29 08:35:18 +0530

    module to reset RFQ / Purchase Order list view

---
+++ nox_purchase_tree_view/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2016-TODAY Linserv Aktiebolag, Sweden (<http://www.linserv.se>).
+#
+##############################################################################
+
+{
+  'name': 'Purchase Order List view',
+  'version': '10.0.0.1',
+  'category': 'NOX',
+  'summary': 'Purchase Order List view',
+  'description': """
+##############################################################
+                NOX Purchase Order List view
+##############################################################
+This module modifies and sets Request for Quotation / Purchase Order list view as :
+Order Number | Customer | Vendor | Consultant | Contract signed | Source Document | Start Date | End Date
+  """,
+  'author': 'Linserv AB',
+  'contributors': ['Riyaj Pathan <rjpathan19@gmail.com>'],
+  'website': 'www.linserv.se/en/',
+  'depends': ['nox_opportunity_order', 'purchase'],
+  'data': [ 
+  		'views/purchase.xml',
+	],
+
+  'application': False,
+  'auto_install': False,
+  'installable': True,
+}
