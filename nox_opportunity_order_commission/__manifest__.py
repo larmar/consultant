214dcfffe094741c37b6e881042cdddd6bba180b
Riyaj Pathan <rjpathan19@gmail.com>
2017-10-01 15:55:59 +0530

    update module description

--- nox_opportunity_order_commission/__manifest__.py
+++ nox_opportunity_order_commission/__manifest__.py
@@ -21,6 +21,11 @@ This module adds following new fields in Opportunity & Quotation/Sales Order vie
     *	Amount per hour
 
 If Commission Payment check box is ticked; "Create Commission Order" button and "Commission Order" tab shows up.
+Commission Order details:
+  * Commission Order is created with Vendor selected and Product(s) of type "Is Commission Product".
+  * Quantity on Commission order line is set to sum of Total Hours for Consultant 1 & Consultant 2 from related Sales Order.
+  * When Vendor bill is created for Commission Order; Bill quantity is the sum of all Vendor bills quantity for all Vendor Bills related to Purchase Order(s); where Purchase Orders & Commission Orders are related to same Sales Order.
+
   """,
   'author': 'Linserv AB',
   'contributors': ['Riyaj Pathan <rjpathan19@gmail.com>'],
