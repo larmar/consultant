29eb676dced11d4323933d68460c63f974c04ca9
Riyaj Pathan <rjpathan19@gmail.com>
2017-10-06 09:41:42 +0530

    fix Commission order Vendor bills quantity

--- nox_opportunity_order_commission/models/account_invoice.py
+++ nox_opportunity_order_commission/models/account_invoice.py
@@ -16,25 +16,37 @@ class AccountInvoice(models.Model):
 
 
     def _prepare_invoice_line_from_po_line(self, line):
-        """Compute Vendor Bill's quantity (vendor bill related to Commission Order only) from Vendor bills related to 
-        purchase orders and these purchase orders related to same Sales Order as Commission Order is related to.
+        """Compute Vendor Bill's quantity (vendor bill related to Commission Order only) so that for the same related SO,
+        all PO's Vendor bills (validated | paid) quantity is equal to the total quantity in all Commission Order Vendor Bills (validated | paid)
         """
         data = super(AccountInvoice, self)._prepare_invoice_line_from_po_line(line)
         context = self.env.context or {}
         if context.get('active_model', '') == 'purchase.order':
-            purchase = self.env['purchase.order'].browse(context.get('active_ids', []))
-            if purchase and purchase.commission_sale_id:
-                po_ids, qty = [], 0.0
-                for commission_sale_id in purchase.commission_sale_id:
-                    for po_rel in commission_sale_id.purchase_line_ids:
-                        po_ids.append(po_rel.order_id)
-                    po_ids = list(set(po_ids))
+            def get_total_qty(po_ids):
+                qty = 0.0
                 for po in po_ids:
                     for bill in po.invoice_ids:
                         if bill.state in ('open', 'paid'):
                             for bill_line in bill.invoice_line_ids:
                                 qty += bill_line.quantity
+                return qty
 
-                data['quantity'] = qty
+            purchase = self.env['purchase.order'].browse(context.get('active_ids', []))
+            if purchase and purchase.commission_sale_id:
+                po_ids, po_bill_qty, commission_po_ids, commission_bill_qty, bill_to_qty = [], 0, [], 0, 0
+                for commission_sale_id in purchase.commission_sale_id:
+                    for po_rel in commission_sale_id.purchase_line_ids:
+                        po_ids.append(po_rel.order_id)
+                    po_ids = list(set(po_ids))
+                    po_bill_qty = get_total_qty(po_ids)
+
+                    for po_rel in commission_sale_id.commission_order_lines:
+                        commission_po_ids.append(po_rel.order_id)
+                    commission_po_ids = list(set(commission_po_ids))
+                    commission_bill_qty = get_total_qty(commission_po_ids)
+
+                    bill_to_qty = po_bill_qty - commission_bill_qty
+
+                data['quantity'] = bill_to_qty
 
         return data
