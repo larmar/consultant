928bdf42aa18b116079881193531967a73620255
Andreas Danielsson <joey_1slipknot@hotmail.com>
2017-10-11 15:20:31 +0200

    Added jquery datetime picker to next_available

--- website_consultant/static/src/js/website_consultant.js
+++ website_consultant/static/src/js/website_consultant.js
@@ -1,3 +1,30 @@
 function confirmTerms() {
     $('.modalDialog').show();
-}
+};
+
+odoo.define('website_event.consultant', function (require) {
+
+    var ajax = require('web.ajax');
+    var Widget = require('web.Widget');
+    var web_editor_base = require('web_editor.base');
+    
+    // Catch registration form event, because of JS for attendee details
+    var ConsultantEditForm = Widget.extend({
+        start: function () {
+            var self = this;
+            var res = this._super.apply(this.arguments).then(function () {
+                $('[name="next_available"]').datetimepicker({});
+            });
+            return res
+        },
+    });
+
+    web_editor_base.ready().then(function () {
+        var consultant_edit_form = new ConsultantEditForm().appendTo($('#consultant_edit_form'));
+    });
+
+    return {
+        ConsultantEditForm: ConsultantEditForm
+    };
+
+});
