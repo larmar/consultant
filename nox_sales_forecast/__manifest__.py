7c1094f02bcbaf450d4c6e9620b32cf6f5b6032e
Riyaj Pathan <rjpathan19@gmail.com>
2018-01-22 05:14:07 +0530

    app: Nox sales forecast (Auto generate "Working Hours by month" records for every calendar year)

---
+++ nox_sales_forecast/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2016-TODAY Linserv Aktiebolag, Sweden (<http://www.linserv.se>).
+#
+##############################################################################
+
+{
+  'name': 'NOX - Sales Forecast',
+  'version': '10.0.0.1',
+  'category': 'NOX',
+  'summary': 'NOX - Sales Forecast',
+  'description': """
+##############################################################
+                NOX - Sales Forecast
+##############################################################
+Sales Forecasting for Cost & Revenue
+	*	Auto generate "Working Hours by month" records for every calendar year
+
+  """,
+  'author': 'Linserv AB',
+  'contributors': ['Riyaj Pathan <rjpathan19@gmail.com>'],
+  'website': 'www.linserv.se/en/',
+  'depends': ['nox_opportunity_order'],
+  'data': [ 
+  		
+  		'security/ir.model.access.csv',
+
+  		'data/data.xml',
+  		'data/scheduler.xml',
+
+  		'views/date_month_view.xml',
+	],
+
+  'application': False,
+  'auto_install': False,
+  'installable': True,
+}
---
+++ nox_sales_forecast/models/date_month_year.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2016-TODAY Linserv Aktiebolag, Sweden (<http://www.linserv.se>).
+#
+##############################################################################
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+from datetime import datetime
+
+MONTHS = {
+	'01': 'January',
+	'02': 'February',
+	'03': 'March',
+	'04': 'April',
+	'05': 'May',
+	'06': 'June',
+	'07': 'July',
+	'08': 'August',
+	'09': 'September',
+	'10': 'October',
+	'11': 'November',
+	'12': 'December'
+}
+class date_month_year(models.Model):
+	_name = "date.month.year"
+	_description = "Years"
+	_order = "month,year"
+
+	year = fields.Char('Year', size=4, required=True)
+	month = fields.Char('Month', size=2, required=True)
+
+	_sql_constraints = [('month_year_uniq', 'unique(year,month)', 'Duplicate Month-Year not allowed!')]
+
+	@api.multi
+	def name_get(self):
+		res = []
+		for rec in self:
+			month = MONTHS[rec.month]
+			res.append((rec.id, ' '.join([month, rec.year])))
+		return res
+
+	@api.model
+	def generate_current_year_months(self):
+		"""Function to generate months of the current year on module installation
+		"""
+		year = datetime.now().year
+		for month in MONTHS:
+			if not self.search([('year','=',year), ('month','=',month)]):
+				rec = self.create({
+						'year': year,
+						'month': month
+					})
+				self.env['date.month.working.hours'].create({
+						'month_year_id': rec.id
+					})
+		return True
+
+class date_month_working_hours(models.Model):
+	_name = "date.month.working.hours"
+	_description = "Working Hours Per Month"
+	_rec_name = 'month_year_id'
+	_order = "year,month"
+
+	month_year_id = fields.Many2one('date.month.year', 'Month', required=True)
+	month = fields.Char(related="month_year_id.month", store=True)
+	year = fields.Char(related="month_year_id.year", store=True)
+	working_hours = fields.Integer('Working Hours')
+
+	@api.constrains('working_hours')
+	def check_working_hours(self):
+		if self.working_hours > 720:
+			raise ValidationError('Invalid Hours!\n\nWorking hours exceeded total hours in a month.')
+		if self.working_hours < 0:
+			raise ValidationError('Invalid Hours!\n\nWorking hours cannot be less than 0.')
