78331a23c03b65f84bc2bc4c62b940f9498509bb
Riyaj Pathan <rjpathan19@gmail.com>
2018-04-21 00:15:05 +0530

    Delivered quantity update in SO for expense product (other than consultant & non-standard product)

--- nox_opportunity_order_purchase/models/sale.py
+++ nox_opportunity_order_purchase/models/sale.py
@@ -58,10 +58,16 @@ class SaleOrder(models.Model):
         """Compute accumulated Delivered Quantity in Sales Order lines with Consultant Product from related PO > Vendor Bills
         """
         for sale in self:
-            product_list, po_ids = {}, []
+            product_list, po_ids, expense_product_list = {}, [], {}
             for line in sale.order_line:
+                # check for consultant (standard) product & non-standard product
                 if line.product_id and line.product_id.consultant_product or line.product_id.non_standard_product and line.product_id.id not in product_list:
                     product_list[line.product_id.id] = 0
+                # check for expense product (by comparing product and unit price from sale order in vendor bill):
+                elif line.product_id and not line.product_id.consultant_product and not line.product_id.non_standard_product:
+                    expense_product_key = '-'.join([str(line.product_id.id), str(line.price_unit)])
+                    if expense_product_key not in expense_product_list:
+                        expense_product_list[expense_product_key] = 0
             temp = [po_ids.append(pol.order_id) for pol in sale.purchase_line_ids]
             for po in po_ids:
                 for bill in po.invoice_ids:
@@ -80,6 +86,26 @@ class SaleOrder(models.Model):
                     line2.write({'qty_delivered': product_list[line2.product_id.id]})
                     lines_freeze.append(line2.product_id.id)
         
+        	# check for expense product (by comparing product and unit price from sale order in vendor bill): update delivered qty
+            for po in po_ids:
+                for bill in po.invoice_ids:
+                    if bill.state in ('open', 'paid'):
+                        for line in bill.invoice_line_ids:
+                            product_key = '-'.join([str(line.product_id.id), str(line.price_unit)])
+                            if product_key in expense_product_list.keys():
+                                qty = line.quantity
+                                if bill.type == 'in_refund':
+                                    qty = -line.quantity
+                                expense_product_list[product_key] += qty
+
+            #update delivered qty for expense product
+            lines_freeze = []
+            for line3 in sale.order_line:
+                line_pkey = '-'.join([str(line3.product_id.id), str(line3.price_unit)])
+                if line_pkey not in lines_freeze and line_pkey in expense_product_list.keys():
+                    line3.write({'qty_delivered': expense_product_list[line_pkey]})
+                    lines_freeze.append(line_pkey)	
+
             # # update quantity on validating refund bill:
             # product_list, po_ids = {}, []
             # for line in sale.order_line:
